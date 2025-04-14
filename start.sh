94f5c4a72e599e86cd4fa2d5a9fa0b5817938307
maylikenoother <ogedengbemary19@gmail.com>
2025-04-14 22:22:02 +0100

    updated tailwind config

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
-# start.sh (new file)
-
 #!/bin/bash
 # Start both frontend and backend services
 
 # Start FastAPI in the background
-uvicorn api.main:app --host 0.0.0.0 --port 8000 &
+uvicorn api.index:app --host 0.0.0.0 --port 8000 &
 
 # Start Next.js frontend
 npm start
